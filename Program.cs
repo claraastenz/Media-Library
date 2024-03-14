45f964cc7452b7775e473b8ccfb94ba1105328ff
claraastenz <castenz18@gmail.com>
2024-03-14 13:45:44 -0500

    [01-01] Initial program

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿﻿using NLog;
+// See https://aka.ms/new-console-template for more information
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+// create instance of Logger
+var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+logger.Info("Program started");
+
+// Movie movie = new Movie
+// {
+//     mediaId = 123,
+//     title = "Greatest Movie Ever, The (2023)",
+//      director = "Jeff Grissom",
+//     // timespan (hours, minutes, seconds)
+//     runningTime = new TimeSpan(2, 21, 23),
+//     genres = { "Comedy", "Romance" }
+// };
+
+// Console.WriteLine(movie.Display());
+// Book book = new Book
+// {
+//     mediaId = 111,
+//     title = "Super Cool Book",
+//     author = "Jeff Grissom",
+//     pageCount = 101,
+//     publisher = "",
+//     genres = { "Suspense", "Mystery" }
+// };
+// Console.WriteLine(book.Display());
+string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
+logger.Info(scrubbedFile);
+MovieFile movieFile = new MovieFile(scrubbedFile);
+
+logger.Info("Program ended");
+
